dd4fd2b2437d3b8b5d547acd07b48babe10f70c5
wlame <4479808+wlame@users.noreply.github.com>
2026-04-25 16:44:09 +0200

    release: bump version to 0.11.0

--- drakkar/__init__.py
+++ drakkar/__init__.py
@@ -1,6 +1,6 @@
 """Drakkar — Kafka subprocess orchestration framework."""
 
-__version__ = '0.10.0'
+__version__ = '0.11.0'
 
 from drakkar.app import DrakkarApp
 from drakkar.cache import CacheScope
