b61c6fb37737827d8afb8dc388c2fd1a459de1d1
wlame <4479808+wlame@users.noreply.github.com>
2026-08-24 08:25:30 +0200

    Bump version to 1.18.0.

--- drakkar/__init__.py
+++ drakkar/__init__.py
@@ -1,6 +1,6 @@
 """Drakkar — Kafka subprocess orchestration framework."""
 
-__version__ = '1.17.1'
+__version__ = '1.18.0'
 
 from drakkar.annotations import AnnotatorLike
 from drakkar.app import DrakkarApp
